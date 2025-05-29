6ec449c7f97734e00011c71e773b816de564d5a6
Matheus Fernandes <matheuz1398@gmail.com>
2025-05-28 22:30:56 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1>hello Wolrd</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
